9e30fdb2ecdcb3833aeaeb2c46bb0b7f99f4f5cc
LEFRANCOIS Maxime <maxime.lefrancois.86@gmail.com>
2017-02-03 18:45:23 +0100

    added figures

--- ssn/one-namespace/figure/make.bat
+++ ssn/one-namespace/figure/make.bat
@@ -1,20 +1,8 @@
 echo off
-del "*.aux"
-del "*.log"
-del "*.out"
-del "*.fdb_latexmk"
-del "*.fls"
-del "*.dvi"
 for %%f in (*.tex) do (
   set "N=%%f"
-  latexmk %%~nf
+  latexmk %%~nf.tex
   dvipdfm %%~nf.dvi
   magick -density 300 %%~nf.pdf -quality 90 %%~nf.png
 
 )
-del "*.aux"
-del "*.log"
-del "*.out"
-del "*.fdb_latexmk"
-del "*.fls"
-del "*.dvi"
